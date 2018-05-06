54017d132c1e92f981f0c14ecfb95917f9696378
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-06 01:10:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = []
+  for (var i = 0; i === 4; i++ ) {
+  emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return emptyArray
+}
