8e3720e80d3468cc5fe6620bebfcb3765bbde381
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-06 01:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var emptyArray = []
 function theBeatlesPlay(musicians, instruments){
+  var emptyArray = []
   for (var i = 0; i === 4; i++ ) {
   emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
   }
